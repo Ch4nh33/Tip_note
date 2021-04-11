b5fc8e62ba403417b4f8365ff3de08000a6b150e
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-04-12 07:14:35 +0900

    Create 2-5.cpp

---
+++ 2-5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(void) 
+{
+    char password[11];
+	cout << "input the password >>"<<endl;
+	while(true)
+	{
+		cout <<"password :";
+		cin >> password;
+		if(strcmp(password,"C++")==0)
+		{
+			cout <<"this program is off"<<endl;
+			break;
+		}
+		else 
+		{
+			cout<<"wrong password!"<<endl;
+		}
+	} 
+	return 0;
+}
