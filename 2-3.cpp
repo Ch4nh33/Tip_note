9d0e6a24d11c4d87d42588243346158a18ad8ec0
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-04-12 06:29:35 +0900

    Create 2-3.cpp

---
+++ 2-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+	int width = 0;
+	int height = 0;
+	cout << "input the width >>";
+	cin >> width;
+	
+	cout << "input the height >>";
+	cin >> height;
+	
+	cout <<"area is "<<width*height<< endl; 
+	return 0;
+}
