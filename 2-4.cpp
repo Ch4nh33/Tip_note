b002138e92df183aac9c679cd8fc00815781ac63
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-04-12 07:06:38 +0900

    Create 2-4.cpp

---
+++ 2-4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+    cout<<"enter the name >> "; 
+	char name[11];
+	cin >> name;
+	cout <<"name is "<<name<<"!"<<endl; 
+	return 0;
+}
